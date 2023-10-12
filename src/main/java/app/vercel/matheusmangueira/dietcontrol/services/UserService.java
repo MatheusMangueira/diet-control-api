238ff9a1ad57f0a9f659bc43581aec089d63c263
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-10-12 18:24:34 -0300

    feat: create UserService

---
+++ src/main/java/app/vercel/matheusmangueira/dietcontrol/services/UserService.java
@@ -0,0 +1,52 @@
+package app.vercel.matheusmangueira.dietcontrol.services;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+import app.vercel.matheusmangueira.dietcontrol.dtos.UserDto;
+import app.vercel.matheusmangueira.dietcontrol.repository.IUserRepository;
+import app.vercel.matheusmangueira.dietcontrol.user.model.UserModel;
+import app.vercel.matheusmangueira.dietcontrol.utils.CalculateTmb;
+import jakarta.validation.Valid;
+
+@Service
+public class UserService {
+
+   @Autowired
+   private IUserRepository userRepository;
+
+   @Autowired
+   private CalculateTmb utils;
+
+   public List<UserModel> getAllUsers() {
+      return this.userRepository.findAll();
+   }
+
+   public UserModel createUser(UserModel userModel) {
+      return this.userRepository.save(userModel);
+   }
+
+   public UserDto findUserById(@Valid UUID id) {
+      var basal = this.utils.BasalCalculate(id);
+      var macros = this.utils.CalculateMacros(id, basal);
+      var user = this.userRepository.findById(id).orElseThrow();
+
+      var newUser = new UserDto(
+            id,
+            user.getName(),
+            user.getSex(),
+            user.getAge(),
+            user.getWeight(),
+            user.getHeight(),
+            user.getActivityLevel(),
+            user.getObjectUser(),
+            basal,
+            macros);
+
+      return newUser;
+   }
+}

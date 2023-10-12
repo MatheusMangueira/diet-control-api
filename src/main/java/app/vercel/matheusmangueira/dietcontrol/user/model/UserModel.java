742ef735a26c0634c3b210bad35c9467db3cda00
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-10-12 18:27:00 -0300

    fix: add table and equalsAndHashCode in UserModal

--- src/main/java/app/vercel/matheusmangueira/dietcontrol/user/model/UserModel.java
+++ src/main/java/app/vercel/matheusmangueira/dietcontrol/user/model/UserModel.java
@@ -6,16 +6,19 @@ import java.util.UUID;
 import org.hibernate.annotations.CreationTimestamp;
 
 import app.vercel.matheusmangueira.dietcontrol.user.enums.ActivityLevel;
+import app.vercel.matheusmangueira.dietcontrol.user.enums.ObjectUser;
 import app.vercel.matheusmangueira.dietcontrol.user.enums.Sex;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -25,6 +28,8 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity(name = "tb_users")
+@Table(name = "tb_users")
+@EqualsAndHashCode(of = "id")
 public class UserModel {
 
    @Id
@@ -38,6 +43,9 @@ public class UserModel {
 
    @Enumerated
    private Sex sex;
+
+   @Enumerated
+   private ObjectUser ObjectUser;
 
    @NotNull
    @Min(1)

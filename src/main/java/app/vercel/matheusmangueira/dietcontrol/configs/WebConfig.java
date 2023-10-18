81dc29af99c6e25bec2f68e179fa0865eea42cf1
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-10-18 16:11:54 -0300

    feat: Create WebConfigs for Cors

---
+++ src/main/java/app/vercel/matheusmangueira/dietcontrol/configs/WebConfig.java
@@ -0,0 +1,20 @@
+package app.vercel.matheusmangueira.dietcontrol.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
+   
+   
+   public void AddCorsMappings(CorsRegistry registry  ){
+      
+      registry.addMapping("/**");
+
+   }
+
+      
+}

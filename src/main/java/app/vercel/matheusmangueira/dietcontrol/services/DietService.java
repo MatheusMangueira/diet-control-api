d8c38fe40adb61c5698b3b6745cb6c0372f7e00e
MatheusMangueira <matheusmangueira16@hotmail.com>
2023-10-18 14:11:30 -0300

    fix: remove console

--- src/main/java/app/vercel/matheusmangueira/dietcontrol/services/DietService.java
+++ src/main/java/app/vercel/matheusmangueira/dietcontrol/services/DietService.java
@@ -22,7 +22,6 @@ public class DietService {
       Optional<DietListModel> dietOptional = this.dietRepository.findById(dietId);
 
       if (dietOptional.isPresent()) {
-         System.out.println("SEGUNDO DATA ");
 
          DietListModel existingDiet = dietOptional.get();
 
@@ -43,57 +42,12 @@ public class DietService {
          }
          this.dietRepository.save(existingDiet);
 
-         System.out.println("TERCEIRO DATA " + data);
          return data;
 
       }
 
-      System.out.println("QUARTO DATA " + dietOptional.isPresent());
       return null;
 
    }
-   // UserModel user = userOptional.get();
-
-   // System.out.println("USER " + user);
-
-   // List<DietListModel> dietListModels = this.dietRepository.findByUser(user);
-
-   // System.out.println("DIET " + dietListModels);
-
-   // Optional<DietListModel> dietOptional = dietListModels.stream().filter(diet ->
-   // diet.getId().equals(userId))
-   // .findFirst(); // Corrigido para usar id
-
-   // System.out.println("dietOptional " + dietOptional);
-
-   // if (dietOptional.isPresent()) {
-   // DietListModel obj = dietOptional.get();
-
-   // System.out.println("SEGUNDO DATA " + obj);
-
-   // if (data.getFoodName() != null) {
-   // obj.setFoodName(data.getFoodName());
-   // }
-   // if (data.getCarb() != null) {
-   // obj.setCarb(data.getCarb());
-   // }
-   // if (data.getProtein() != null) {
-   // obj.setProtein(data.getProtein());
-   // }
-   // if (data.getFat() != null) {
-   // obj.setFat(data.getFat());
-   // }
-   // if (data.getQuantity() != null) {
-   // obj.setQuantity(data.getQuantity());
-   // }
-   // this.dietRepository.save(obj);
-
-   // System.out.println("TERCEIRO DATA " + obj);
-   // return obj;
-
-   // }
-
-   // System.out.println("QUARTO DATA " + data);
-   // return null;
 
 }
